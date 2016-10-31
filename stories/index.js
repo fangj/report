565efedc3d5541deb75be7971db2e692c553a183
Fang Jian <fangjianmail@126.com>
2016-10-31 14:17:05 +0800

    登陆模块

--- stories/index.js
+++ stories/index.js
@@ -12,3 +12,13 @@ storiesOf('Button', module)
   .add('with pubsub', () => (
     <button onClick={()=>PubSub.publish('msg.error','something wrong')}>😀 😎 👍 💯</button>
   ));
+
+import UserLogin from '../components/login/UserLogin';
+
+storiesOf('Login ', module)
+  .add('success', () => (
+  	<UserLogin login={()=>{return Promise.resolve()}}/>
+  ))
+  .add('fail', () => (
+  	<UserLogin login={()=>{return Promise.reject()}}/>
+  ));
