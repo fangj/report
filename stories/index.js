250fdf4cef8b9f709dbd6d2b39ef6531b60b658f
Fang Jian <fangjianmail@126.com>
2016-10-31 12:50:17 +0800

    add toast

--- stories/index.js
+++ stories/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
 
+
 storiesOf('Button', module)
   .add('with text', () => (
   	<button type="button" className="btn btn-success" onClick={action('clicked')}>Success</button>
   ))
   .add('with some emoji', () => (
     <button onClick={()=>toastr.info('Are you the 6 fingered man?')}>😀 😎 👍 💯</button>
+  ))
+  .add('with pubsub', () => (
+    <button onClick={()=>PubSub.publish('msg.error','something wrong')}>😀 😎 👍 💯</button>
   ));
